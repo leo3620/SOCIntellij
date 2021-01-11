1286286bb6bb8fc6d00c4672e53c5e6f5b3eb68a
Charlotte <chagame@outlook.fr>
2021-01-11 16:18:41 +0100

    RechercheSynchroneAbstraite

--- src/main/java/modele/RechercheSynchroneAbstraite.java
+++ src/main/java/modele/RechercheSynchroneAbstraite.java
@@ -4,10 +4,17 @@ import java.util.Optional;
 
 import javax.ws.rs.client.Client;
 
+import com.sun.istack.Pool;
 import infrastructure.jaxrs.HyperLien;
+import infrastructure.jaxrs.LienVersRessource;
+import infrastructure.jaxrs.Outils;
 
 public abstract class RechercheSynchroneAbstraite implements AlgorithmeRecherche {
 
-	abstract protected Optional<HyperLien<Livre>> rechercheSync(HyperLien<Bibliotheque> h, Livre l, Client client);
+	protected Optional<HyperLien<Livre>> rechercheSync(HyperLien<Bibliotheque> h, Livre l, Client client){
+		Outils.afficherInfoTache("recherche synchrone");
+		Bibliotheque bibli = LienVersRessource.proxy(client, h, Bibliotheque.class);
+		return bibli.chercher(l);
+	}
 
 }

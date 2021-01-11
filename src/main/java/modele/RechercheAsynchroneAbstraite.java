27377cf3bb82055450d7859cbb46927a9ca550b0
Charlotte <chagame@outlook.fr>
2021-01-11 16:51:05 +0100

    rechercheAsychroneAbstraite

--- src/main/java/modele/RechercheAsynchroneAbstraite.java
+++ src/main/java/modele/RechercheAsynchroneAbstraite.java
@@ -3,15 +3,38 @@ package modele;
 import java.util.Optional;
 import java.util.concurrent.Future;
 
+import configuration.JAXRS;
 import infrastructure.jaxrs.HyperLien;
+import infrastructure.jaxrs.LienVersRessource;
+import infrastructure.jaxrs.Outils;
+import infrastructure.langage.Types;
+
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.InvocationCallback;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
 
 public abstract class RechercheAsynchroneAbstraite implements AlgorithmeRecherche {
 
-	abstract protected Future<Optional<HyperLien<Livre>>> rechercheAsync(HyperLien<Bibliotheque> h, Livre l, Client client);
+	protected Future<Optional<HyperLien<Livre>>> rechercheAsync(HyperLien<Bibliotheque> h, Livre l, Client client){
+		WebTarget wt = client.target(h.getUri());
+		return wt.path(JAXRS.SOUSCHEMIN_ASYNC)
+				.request()
+				.accept(JAXRS.TYPE_MEDIATYPE)
+				.async()
+				.put(Entity.entity(l, JAXRS.TYPE_MEDIATYPE), Types.typeRetourChercherAsync());
 
-	abstract protected Future<Optional<HyperLien<Livre>>> rechercheAsyncAvecRappel(
+	}
+
+	protected Future<Optional<HyperLien<Livre>>> rechercheAsyncAvecRappel(
 			HyperLien<Bibliotheque> h, Livre l, Client client,  
-			InvocationCallback<Optional<HyperLien<Livre>>> retour);
+			InvocationCallback<Optional<HyperLien<Livre>>> retour){
+		WebTarget wt = client.target(h.getUri());
+		return wt.path(JAXRS.SOUSCHEMIN_ASYNC)
+				.request()
+				.accept(JAXRS.TYPE_MEDIATYPE)
+				.async()
+				.put(Entity.entity(l, JAXRS.TYPE_MEDIATYPE), retour);
+	}
 }
